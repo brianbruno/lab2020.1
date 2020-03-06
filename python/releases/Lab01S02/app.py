67f9ca922d1e069d77fba36a07f15aae0be1b289
Brian Bruno <30503728+brianbruno@users.noreply.github.com>
2020-03-05 21:26:34 -0300

    Update app.py

--- python/releases/Lab01S02/app.py
+++ python/releases/Lab01S02/app.py
@@ -1,4 +1,5 @@
 import requests
+import time
 from json import dump
 from json import loads
 
@@ -14,7 +15,7 @@ def run_query(json, headers): # A simple function to use requests.post to make t
 
 query = """
 {
-  search(query:"stars:>100", type:REPOSITORY, first:20 {AFTER}){
+  search(query:"stars:>100", type:REPOSITORY, first:10 {AFTER}){
     pageInfo{
         hasNextPage
         endCursor
@@ -75,27 +76,34 @@ nodes = result['data']['search']['nodes']
 next_page  = result["data"]["search"]["pageInfo"]["hasNextPage"]
 
 #paginating
-while (next_page and total_pages < 20):
-    total_pages += 1
-    cursor = result["data"]["search"]["pageInfo"]["endCursor"]
-    next_query = query.replace("{AFTER}", ", after: \"%s\"" % cursor)
-    json["query"] = next_query
-    result = run_query(json, headers)
-    nodes += result['data']['search']['nodes']
-    next_page  = result["data"]["search"]["pageInfo"]["hasNextPage"]
+while (next_page and total_pages < 100):
+    try:
 
-#saving data
-for node in nodes:
-    if (node['primaryLanguage'] is None):
-      primaryLanguage  = 'None'
-    else:
-      primaryLanguage  = node['primaryLanguage']['name']
+      print("Paginas: "+str(total_pages))
+      time.sleep(30)
+      total_pages += 1
+      cursor = result["data"]["search"]["pageInfo"]["endCursor"]
+      next_query = query.replace("{AFTER}", ", after: \"%s\"" % cursor)
+      json["query"] = next_query
+      result = run_query(json, headers)
+      # nodes += result['data']['search']['nodes']
+      next_page  = result["data"]["search"]["pageInfo"]["hasNextPage"]
 
-    with open("repos.csv", 'a') as the_file:
-        the_file.write(node['nameWithOwner'] + "," + 
-          node['createdAt'] + "," + 
-          node['updatedAt'] + "," + 
-          str(node['acceptedsPullRequests']['totalCount']) + "," + 
-          str(node['releases']['totalCount']) + "," +
-          str(node['closedIssues']['totalCount']) + "," + 
-          str(node['totalIssues']['totalCount']) + "," + "\n") 
+      #saving data
+      for node in result['data']['search']['nodes']:
+        if (node['primaryLanguage'] is None):
+          primaryLanguage  = 'None'
+        else:
+          primaryLanguage  = node['primaryLanguage']['name']
+
+        with open("repos.csv", 'a') as the_file:
+          the_file.write(node['nameWithOwner'] + "," + 
+            node['createdAt'] + "," + 
+            node['updatedAt'] + "," + 
+            str(node['acceptedsPullRequests']['totalCount']) + "," + 
+            str(node['releases']['totalCount']) + "," +
+            str(node['closedIssues']['totalCount']) + "," + 
+            str(node['totalIssues']['totalCount']) + "\n") 
+    except:
+      print('Tentando novamente')
+      time.sleep(60)
